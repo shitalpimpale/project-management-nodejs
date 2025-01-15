f5c9ec09ed7631261f3f9228d996c965b0d32e19
Shital Pimpale <pimpale.shital@gmail.com>
2025-01-15 10:35:13 +0530

    code cleaning

--- routes/taskTagRoutes.js
+++ routes/taskTagRoutes.js
@@ -2,9 +2,4 @@ const express = require('express');
 const { createTaskTag, deleteTaskTag, updateTaskTag } = require('../controllers/taskTagController');
 
 const router = express.Router();
-
-// router.post('/createTaskTag', createTaskTag);
-// router.put('/updateTaskTag', updateTaskTag);
-// router.delete('/deleteTaskTag/:id', deleteTaskTag);
-
 module.exports = router;
